5d3e984930660cf06efcb2bd0f7f75baf3d202c8
setcreed <cwzdzg@foxmail.com>
2024-03-19 18:08:05 +0800

    设置sql优先

--- pkg/data/store.go
+++ pkg/data/store.go
@@ -2,12 +2,18 @@ package data
 
 import (
 	"context"
-	v1 "github.com/setcreed/store-core/api/store_service/v1"
+	"fmt"
+
 	"gorm.io/gorm"
+
+	v1 "github.com/setcreed/store-core/api/store_service/v1"
+	"github.com/setcreed/store-core/cmd/app/config"
 )
 
 type StoreInterface interface {
-	QueryByTableName(ctx context.Context, tableName string, params *v1.SimpleParams) ([]map[string]interface{}, error)
+	QueryByTableName(ctx context.Context, sqlConfig *config.SQLConfig, params *v1.SimpleParams) ([]map[string]interface{}, error)
+	QueryBySql(ctx context.Context, sqlConfig *config.SQLConfig, params *v1.SimpleParams) ([]map[string]interface{}, error)
+	Query(ctx context.Context, sqlConfig *config.SQLConfig, params *v1.SimpleParams) ([]map[string]interface{}, error)
 }
 
 type store struct {
@@ -20,13 +26,32 @@ func newStore(db *gorm.DB) StoreInterface {
 	}
 }
 
-func (s *store) QueryByTableName(ctx context.Context, tableName string, params *v1.SimpleParams) ([]map[string]interface{}, error) {
+func (s *store) QueryByTableName(ctx context.Context, sqlConfig *config.SQLConfig, params *v1.SimpleParams) ([]map[string]interface{}, error) {
 	dbResult := make([]map[string]interface{}, 0)
-	db := s.db.Table(tableName)
+	db := s.db.Table(sqlConfig.Table)
 	for k, v := range params.Params.AsMap() {
 		db = db.Where(k, v)
 	}
 	db = db.Find(&dbResult)
 
+	return dbResult, db.Error
+}
+
+// Query 设置sql优先，如果写了sql和table，优先执行sql
+func (s *store) Query(ctx context.Context, sqlConfig *config.SQLConfig, params *v1.SimpleParams) ([]map[string]interface{}, error) {
+	if sqlConfig.Sql == "" || sqlConfig.Table == "" {
+		return nil, fmt.Errorf("error sql or table ")
+	}
+	if sqlConfig.Sql != "" {
+		return s.QueryBySql(ctx, sqlConfig, params)
+	}
+
+	return s.QueryByTableName(ctx, sqlConfig, params)
+
+}
+
+func (s *store) QueryBySql(ctx context.Context, sqlConfig *config.SQLConfig, params *v1.SimpleParams) ([]map[string]interface{}, error) {
+	dbResult := make([]map[string]interface{}, 0)
+	db := s.db.Raw(sqlConfig.Sql, params.Params.AsMap()).Find(&dbResult)
 	return dbResult, db.Error
 }
